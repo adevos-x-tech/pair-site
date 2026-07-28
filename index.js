1e5e09314baeef099a6b392d015c282a83a8288a
Adevos <adevostech@gmail.com>
2026-07-28 21:06:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const bodyParser = require("body-parser");
-
 __path = process.cwd();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 8000;
 
-let server = require('./qr');
-let code = require('./pair');
+const qrRouter = require('./qr');
+const pairRouter = require('./pair');
+const sessionRouter = require('./session');
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-app.use(express.static(__path));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__path));
 
-// Serve Logo Image
-app.get('/logo.png', (req, res) => {
-    res.sendFile(path.join(__path, 'logo.png'));
+app.use('/qr', qrRouter);
+app.use('/code', pairRouter);
+app.use('/session', sessionRouter);
+
+app.use('/pair', async (req, res) => {
+    res.sendFile(__path + '/pair.html');
 });
 
-// Routers for Backend Services
-app.use('/qr', server);
-app.use('/code', code);
-
-// Frontend HTML Pages
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(path.join(__path, 'pair.html'));
-});
-
-app.use('/', async (req, res, next) => {
-    res.sendFile(path.join(__path, 'main.html'));
+app.use('/', async (req, res) => {
+    res.sendFile(__path + '/main.html');
 });
 
 app.listen(port, () => {
-    console.log(`📡 Connected on http://localhost:` + port);
+    console.log(`Connected on http://localhost:` + port);
 });
 
 module.exports = app;
